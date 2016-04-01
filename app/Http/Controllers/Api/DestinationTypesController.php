ab9cbf7dd0e2ffc5e8ddd9feaf557a511b9518e1
bhaway <bhaway.mca@gmail.com>
2016-04-01 17:41:08 +0530

    destination types

---
+++ app/Http/Controllers/Api/DestinationTypesController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Response;
+use App\Models\DestinationTypes;
+
+class DestinationTypesController extends Controller 
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() 
+    {
+         $destination = DestinationTypes::all();
+		 return $destination->toJson();
+		 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+		$destination = DestinationTypes::find($id);
+		return $destination->toJson();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+		
+    }
+}
--- app/Http/routes.api.php
+++ app/Http/routes.api.php
@@ -2,3 +2,4 @@
 Route::resource('api/visitors', 'VisitorsController');
 Route::resource('api/cards', 'CardsController');
 Route::resource('api/cards.items', 'DestinationCardItemsController');
+Route::resource('api/destination', 'DestinationTypesController');
