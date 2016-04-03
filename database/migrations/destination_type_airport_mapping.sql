cdf78f55819c08ff058a322571a165d15a3c3445
Nilay <nilayanand@gmail.com>
2016-04-03 13:03:13 +0530

    Fix migration issue

--- database/migrations/destination_type_airport_mapping.sql
+++ database/migrations/destination_type_airport_mapping.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `destination_type_airport_mapping` (
   `destination_type_id` int(11) NOT NULL,
   `destination_id` int(11) NOT NULL,
   `created_at` timestamp NOT NULL,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=50 ;
 
