357844f31dbc6ced24cf063b6f19f3a1d24205ad
Nilay <nilayanand@gmail.com>
2016-04-10 00:19:38 +0530

    destination card items

--- database/migrations/2016_04_09_093908_update_destination_card_items.php
+++ database/migrations/2016_04_09_093908_update_destination_card_items.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateDestinationCardItems extends Migration
+class UpdateDestinationCardItems2 extends Migration
 {
     /**
      * Run the migrations.
