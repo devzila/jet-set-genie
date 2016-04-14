7aae1a7854c7215439db346cc892185b34937aa1
kawalbabra <kawal@ideapps.in>
2016-04-14 16:55:28 +0530

    design change on favorites page.

--- resources/views/favorites.blade.php
+++ resources/views/favorites.blade.php
@@ -8,54 +8,63 @@
 @section('content') 
 <section class="app-content container-fluid" data-ng-controller="ctrlFavorites">
 
-<div class="row row-centered favorites-section"  >
-    <div class="row-centered serch-part">
+    <div class="row row-centered favorites-section">
+        <div class="row-centered serch-part">
+            <div class="col-lg-7 col-md-7 col-sm-9 col-xs-12 col-centered">
+                <h3 class="lobster-font text-center">@{{ pagetitle }}</h3>
+                @include('shared.searchbox')
+            </div>
+        </div>
+        <div class="hr"></div>
         <div class="col-lg-7 col-md-7 col-sm-9 col-xs-12 col-centered">
-            <h3 class="lobster-font text-center">@{{ pagetitle }}</h3>
-            @include('shared.searchbox')
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="pull-left back">
+                        <a href="/">
+                            <i class="fa fa-angle-left"></i>
+                            <span>BACK TO RESULTS</span>
+                        </a>
+                    </div>
+                    <div class="pull-right fav-share">
+                        <button class="btn btn-default jsg-submit" type="submit">SHARE</button>
+                    </div>
+                </div>
+            </div>
+            <div class="row favorite-container">
+                <div class="col-md-3 col-sm-4 col-xs-6 list-box destinationCard" data-ng-repeat="favorite in favorites" ng-init="parentIndex = $index">
+                    <div class="box clearfix" data-ng-style="{ background:  randomcolor }">
+                        <div class="box-detail">
+                            <a class="setFavorite" data-ng-click="deleteFavorite( $index )"><i class="fa fa-star"></i></a>
+                            <a class="lnkFlights" data-ng-click="showFlights( record.airport_code, record.display_name, record.id );">
+                                <h5><span>@{{ favorite.display_name }}</span></h5>
+                                <div class="box-info">
+                                    <p>
+                                        <span>
+                                            Cheapest
+                                            <span class="cardinfo">@{{ favorite.fare }}</span>
+                                        </span>
+                                        <span>
+                                            Shortest
+                                            <span data-ng-bind-html="favorite.duration" class="cardinfo"></span>
+                                        </span>
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="blank-bg" data-ng-repeat="flight in favorite.flights">
+                            <a class="removeicon" data-ng-click="deleteflight(parentIndex, $index)" title="Remove Flight"> <i class="fa fa-times"></i> </a>
+                            <div class="box-info">
+                                <h5>FLIGHT</h5>
+                                <p>@{{ flight.price + " " + flight.name }}</p>
+                                <p>@{{ flight.departDate }}</p>
+                            </div>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="hr"></div>
-	<div class="col-lg-7 col-md-7 col-sm-9 col-xs-12 col-centered">
-            <div class="row">
-				<div class="col-xs-12">
-					<div class="pull-left back">
-						<a href="/">
-						<i class="fa fa-angle-left"></i>
-						<span>BACK TO RESULTS</span></a>                                    
-					</div>
-					<div class="pull-right fav-share">
-						<button class="btn btn-default jsg-submit" type="submit">SHARE</button>
-					</div>
-				</div>
-			</div>
-			<div class="row favorite-container">
-				<div class="col-md-3 col-sm-4 col-xs-6 list-box" data-ng-repeat="favorite in favorites" ng-init="parentIndex = $index" >
-                    <div class="box clearfix" data-ng-style="{ background:  randomcolor }">
-						<div class="box-detail">
-                            <a class="setFavorite" data-ng-click="deleteFavorite( $index )" ><i class="fa fa-star"></i></a>
-                            <h5>@{{ favorite.display_name }}</h5>
-							<div class="box-info">
-							<p>Shortest Flight</p>
-                                <p>@{{ favorite.duration }}</p>
-							<p>Chepest Flight</p>
-                                <p>@{{ favorite.fare }}</p>
-							</div>
-						</div>
-						<div class="blank-bg" data-ng-repeat="flight in favorite.flights"> 							
-							<a class="removeicon" data-ng-click="deleteflight(parentIndex, $index)" title="Remove Flight" > <i class="fa fa-times"></i> </a>
-							<div class="box-info">                                           
-							<h5>FLIGHT</h5>
-							<p>@{{ flight.price + " " + flight.name }}</p>
-							<p>@{{ flight.departDate }}</p>                                            
-							</div>
-						</div>
-					<div class="clear"></div>
-					</div>
-				</div>
-		</div>
-	</div>
-</div>
 </section>
 @stop
  
